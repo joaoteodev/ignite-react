e5148dc6ae67c970f791bce8bf66c7b973fba6c3
João Pedro <joaodev84@gmail.com>
2022-08-25 20:16:22 -0300

    Setting up TypeScript

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
-  presets: ["@babel/preset-env", [
-    "@babel/preset-react", {
-      runtime: 'automatic'
-    }
-  ]]
+  presets: [
+    "@babel/preset-env",
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic"
+      }
+    ]
+  ]
 };
